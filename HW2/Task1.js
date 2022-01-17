0cc7f9c2b15c498c99d2f9a8a4ee3a920e70309b
Aleksandr Tkachenko <dn030475taa1@gmail.com>
2022-01-18 00:17:49 +0200

    Task 1 done

--- HW2/Task1.js
+++ HW2/Task1.js
@@ -1,33 +1,6 @@
-console.log('while');
-var i=1;
-while (i<3) {
-    console.log (i);
-    i++;
-}
+var a=2; //example
+var b=3; //example
 
-console.log('do...while');
-var i=1;
-do {
-    console.log(i);
-    i++;
-} while (i<3);
+var result=(a+b < 4)? true : false
 
-console.log('for');
-var i;
-for (i=1; i<3; i++) {
-    console.log(i);
-}
-
-console.log('for in');
-var arr=[1, 2, 3];
-var i;
-for (i in arr) {
-    console.log(i);
-}
-
-console.log('for of');
-var arr=[1, 2, 3];
-var i;
-for (i of arr) {
-    console.log(i);
-}
+var result=(a+b < 4)? console.log(true) : console.log(false) //example
