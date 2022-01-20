dba7edf04d4c46ef64a419db3cf14c25e7c60691
Aleksandr Tkachenko <dn030475taa1@gmail.com>
2022-01-21 01:17:27 +0200

    Task 2 fixed

--- HW3/Task2.js
+++ HW3/Task2.js
@@ -6,20 +6,26 @@ f(1,1,1,1,1,1,1,1); // 8
 f(1,2,'s',4); // Error: all parameters type should be a Number
 */
 
-function sum(a, b, c, d) {
+function sum() {
     let result;
-    if (typeof a === 'number' && typeof b === 'number' && typeof c === 'number' && typeof d === 'number') {
-        result = a + b + c + d;
-    } else {
-        throw new Error('all parametrs should be a number');
-    }
-    return result;
+    a=0;
+    var args = [].slice.call(arguments);
+    console.log(args);
+    var i;
+    for (i of args) {
+        if (typeof i === 'number') {
+            a=a+i;
+        } else {
+            throw new Error('all parametrs should be a number');
+        }        
+    }    
+    return result=a;
 }
 
-a=2;
+a=1;
 b=2;
-c=2;
-d=2;
+c=3;
+d=4;
 
 var result = sum(a, b, c, d);
-console.log(result);
+console.log('Сумма всех параметров ' + result);
