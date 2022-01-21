047727ff016c087d89317869039dd1e7939546a2
Aleksandr Tkachenko <dn030475taa1@gmail.com>
2022-01-21 19:41:46 +0200

    Task 7 done

---
+++ HW3/Task7.js
@@ -0,0 +1,38 @@
+/*Сделайте функцию `getDivisors`, которая параметром принимает число и возвращает массив его делителей
+(чисел, на которое делится данное число начиная от 1 и заканчивая самим собой).
+Данная функция должна обязательно содержать проверку входного параметра, потому что принимать она может только число больше 0.
+getDivisors(12); // [1, 2, 3, 4, 6, 12]
+getDivisors('Content'); // Error: parameter type is not a Number
+getDivisors(0); // Error: parameter can't be a 0 */
+
+a = 12;
+b = 0;
+c = 'Content';
+var arr= [];
+
+function getDivisors () {
+    var i;
+    var j;
+    for (i of arguments) {
+        if (typeof i === 'number') {            
+            if (i === 0) {
+                throw new Error ('Error: parameter can\'t be a 0');
+            } else {
+                for (j=1; j<=i ;j++) {
+                    if (i%j === 0) {
+                        for (k of arguments) {
+                            arr.push(j)
+                        }
+                    }
+                }
+            }
+        } else {
+            throw new Error ('Error: parameter type is not a Number');
+        }
+    }
+
+}
+
+getDivisors (a);
+console.log('Для числа ' + a + ' массив делителей ниже:');
+console.log(arr);
