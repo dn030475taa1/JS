9cf2299170267ae8233f2ffce0d7101f660f4eba
Aleksandr Tkachenko <dn030475taa1@gmail.com>
2022-01-29 00:58:36 +0200

    Task 7 done

---
+++ HW4/Task7.js
@@ -0,0 +1,21 @@
+/*Код ниже получает из массива строк новый массив, содержащий их длины:
+
+var arr = ['abcd', 'abcde', 'ab', 'abc'];
+
+var arrLength = [];
+for (var i = 0; i < arr.length; i++) {
+arrLength[i] = arr[i].length;
+}
+
+console.log( arrLength ); // 4,5,2,3
+Перепишите выделенный участок:
+- используйте вместо цикла `for` метод `map`*/
+
+var arr = ['abcd', 'abcde', 'ab', 'abc'];
+var arrLength = [];
+
+arrLength=arr.map(function(length) {
+    return length.length;
+});
+
+console.log( arrLength ); // 4,5,2,3
