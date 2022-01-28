08bd5b93f81b9b663b15f967bc9a8ee7eb82e30b
Aleksandr Tkachenko <dn030475taa1@gmail.com>
2022-01-28 17:17:14 +0200

    Task 2 done

---
+++ HW4/Task2.js
@@ -0,0 +1,34 @@
+/*Создать имплементацию функции `filter`, логика работы такая же как и у родного метода.
+Функция должна содержать проверки:
+- Первый параметр обязателен и может принимать только массив
+- Второй параметр обязателен и может принимать только функцию
+const arr = [1,2,3];
+filter(arr, function(item, i, arr) {});
+*/
+//'use strict'
+
+const arr = [10,-2,300];
+
+function filter(arr, callback) {
+    var newArray=[];
+    if (!Array.isArray(arr)) {
+        throw new Error('Error: parametr is not an array');
+    }
+    if (typeof callback !=='function') {
+        throw new Error('Error: parametr is not a function');
+    }
+    
+    for (var i=0; i<arr.length; i++) {
+        if(callback(arr[i], i, arr)) {
+           newArray.push(arr[i]);                   
+        }
+    }
+    return newArray;   
+}
+
+
+var filtered=filter(arr, function(item, i, arr) {
+    return item>0;
+    console.log('Индекс ' + item + ': Значение ' + i + ' из массива [' + arr + ']');
+});
+console.log(filtered);
