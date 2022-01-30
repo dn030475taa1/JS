9bfcd16f9ebedf285c4e9e67e004f067a1bfe4c7
Aleksandr Tkachenko <dn030475taa1@gmail.com>
2022-01-30 02:25:41 +0200

    Task 8 done

---
+++ HW4/Task8.js
@@ -0,0 +1,56 @@
+/*Напишите функцию `f` . Данная функция принимает один параметр: одноуровневый или многоуровневый массив. Возвращает данная функция сумму всех элементов на всех уровнях.
+Обратите внимание что функция должна возвращать число 0, если при проходе всех уровней не было найдено ни одного числа.
+Функция должна содержать проверки:
+- Первый параметр обязателен и может принимать только массив
+- Генерировать ошибку если на каком то уровне было найдено не число и не массив
+
+const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
+f(arr); // 12
+const arr2 = [[[[[1,2]]]]];
+f(arr2); // 3
+const arr3 = [[[[[1,2]]],2],1];
+f(arr3); // 6
+const arr4 = [[[[[]]]]];
+f(arr4); // 0
+const arr5 = [[[[[],3]]]];
+f(arr5); // 3*/
+
+function f(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('Error: parameter is not an array!');
+    }
+    if (typeof arguments!=='number' && !Array.isArray(arr)) {
+        throw new Error('Error: parametr is not a number and is not an array');
+    }
+    var newArray=arr.flat(Infinity);
+    if (typeof newArray.length===0) {
+        return 0;
+    }
+    var resultInner = newArray.reduce(function(sum, current) {
+        if (typeof sum!=='number' || typeof current!=="number") {
+            throw new Error('Error: parametr is not a number');
+        }
+        return sum + current;
+    },0)
+    return resultInner;
+}
+
+const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
+var result = f(arr); // 12
+console.log('Сумма всех элементов для массива arr = ' + result);
+
+const arr2 = [[[[[1,2]]]]];
+var result = f(arr2); // 3
+console.log('Сумма всех элементов для массива arr2 = ' + result);
+
+const arr3 = [[[[[1,2]]],2],1];
+var result = f(arr3); // 6
+console.log('Сумма всех элементов для массива arr3 = ' + result);
+
+const arr4 = [[[[[]]]]];
+var result = f(arr4); // 0
+console.log('Сумма всех элементов для массива arr4 = ' + result);
+
+const arr5 = [[[[[],3]]]];
+var result = f(arr5); // 3
+console.log('Сумма всех элементов для массива arr5 = ' + result);
